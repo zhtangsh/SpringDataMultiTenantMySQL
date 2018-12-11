e397fc029f764cd456e384bb9cb00d8a19d734e5
Zihan Tang <mrzihan.tang@gmail.com>
2018-12-11 15:33:35 +0800

    auto generate schemas for all datasources

--- src/main/java/com/zhtangsh/SpringDataMultiTenantMySQL/tenant/config/DataSourceBasedMultiTenantConnectionProviderImpl.java
+++ src/main/java/com/zhtangsh/SpringDataMultiTenantMySQL/tenant/config/DataSourceBasedMultiTenantConnectionProviderImpl.java
@@ -4,16 +4,20 @@ import com.zhtangsh.SpringDataMultiTenantMySQL.config.EnnContext;
 import com.zhtangsh.SpringDataMultiTenantMySQL.master.model.MasterTenant;
 import com.zhtangsh.SpringDataMultiTenantMySQL.master.repository.MasterTenantRepository;
 import com.zhtangsh.SpringDataMultiTenantMySQL.util.DataSourceUtil;
+import org.hibernate.cfg.Environment;
 import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author Created by Zihan Eric Tang(mrzihan.tang@gmail.com) on 2018/12/11
@@ -33,7 +37,9 @@ public class DataSourceBasedMultiTenantConnectionProviderImpl extends AbstractDa
             List<MasterTenant> masterTenants = masterTenantRepository.findAll();
             logger.info("selectAnyDataSource() --  total tenants: {}", masterTenants.size());
             for (MasterTenant masterTenant : masterTenants) {
-                dataSourceMap.put(masterTenant.getTenantId(), DataSourceUtil.createConfiguredDataSource(masterTenant));
+                DataSource dataSource = DataSourceUtil.createConfiguredDataSource(masterTenant);
+                genSchemas(dataSource);
+                dataSourceMap.put(masterTenant.getTenantId(), dataSource);
             }
         }
         return this.dataSourceMap.values().iterator().next();
@@ -46,9 +52,35 @@ public class DataSourceBasedMultiTenantConnectionProviderImpl extends AbstractDa
             List<MasterTenant> masterTenants = masterTenantRepository.findAll();
             logger.info("selectDataSource() -- total tenants:" + masterTenants.size());
             for (MasterTenant masterTenant : masterTenants) {
-                this.dataSourceMap.put(masterTenant.getTenantId(), DataSourceUtil.createConfiguredDataSource(masterTenant));
+                DataSource dataSource = DataSourceUtil.createConfiguredDataSource(masterTenant);
+                genSchemas(dataSource);
+                this.dataSourceMap.put(masterTenant.getTenantId(), dataSource);
             }
         }
         return this.dataSourceMap.get(tenantIdentifier);
+    }
+
+
+    private void genSchemas(DataSource dataSource) {
+        logger.info("Start the configuration of entityManagerFactory for datasource {}", dataSource);
+        LocalContainerEntityManagerFactoryBean emfBean = new LocalContainerEntityManagerFactoryBean();
+        emfBean.setDataSource(dataSource);
+        emfBean.setPackagesToScan(
+                new String[]{
+                        "com.zhtangsh.SpringDataMultiTenantMySQL.tenant.model",
+                        "com.zhtangsh.SpringDataMultiTenantMySQL.tenant.repository"
+                }
+        );
+        emfBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emfBean.setPersistenceUnitName(dataSource.toString());
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        properties.put(Environment.SHOW_SQL, true);
+        properties.put(Environment.FORMAT_SQL, true);
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        emfBean.setJpaProperties(properties);
+        logger.info("End pf entityManagerFactory {} setup", dataSource);
+        emfBean.afterPropertiesSet();
+        emfBean.destroy();
     }
 }
